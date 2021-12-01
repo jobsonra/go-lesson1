44fcf7961887b92d0fed62eaee6191c21298c959
sonjob <jobson.ra@outlook.com>
2021-12-01 10:05:36 -0300

    build: create and test transaction entity

---
+++ entity/transaction_test.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransactionWithAmountGreaterThan1000(t *testing.T) {
+	transaction := NewTransaction()
+	transaction.ID = "1"
+	transaction.AccountID = "1"
+	transaction.Amount = 2000
+	error := transaction.isValid()
+	assert.Error(t, error)
+	assert.Equal(t, "you dont have limit for this transaction", error.Error())
+}
+
+func TestTransactionWithAmountLesserThan1(t *testing.T) {
+	transaction := NewTransaction()
+	transaction.ID = "1"
+	transaction.AccountID = "1"
+	transaction.Amount = 0
+	error := transaction.isValid()
+	assert.Error(t, error)
+	assert.Equal(t, "the amount must be greater than 1", error.Error())
+}
